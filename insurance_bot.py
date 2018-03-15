df6a6974b453b3aef62ddcb06357fb764f252558
Megan Kingon <megan.kingon@takealot.com>
2018-03-15 13:42:45 +0200

    Add base request

--- insurance_bot.py
+++ insurance_bot.py
@@ -10,24 +10,37 @@ client = insurance.Client()
 
 def get_phone_brands(request):
     phone_brands = client.gadgets.list_phone_brands()
-    phone_brands_hash = {brand: brand for brand in phone_brands}
+    return response_object(' '.join(phone_brands))
+
+
+def response_object(speech):
     resp = {
-        "speech": ' '.join(phone_brands),
+        "speech": speech,
         "displayText": "This text probably won't appear anywhere",
     }
     return Response(body=json.dumps(resp))
 
 
-def hello_world(request):
-    return Response('hello world!')
+def check_phone_brand(request):
+    phone_brands = client.gadgets.list_phone_brands()
+    return response_object(' '.join(phone_brands))
+
+
+def compute_base_request(request):
+    print(request.json_body.get('result').get('metadata').get('intentName'))
+    intent = request.json_body.get('result', {}).get('metadata', {}).get('intentName')
+    if intent == 'phone_brand':
+        get_phone_brands(request)
+    else:
+        response_object('No intent given')
 
 if __name__ == '__main__':
     with Configurator() as config:
         config.add_route('phone_brands', '/phone_brands')
         config.add_view(get_phone_brands, route_name='phone_brands')
 
-        config.add_route('hello_world', '/')
-        config.add_view(hello_world, route_name='hello_world')
+        config.add_route('base_request', '/')
+        config.add_view(compute_base_request, route_name='base_request')
 
         app = config.make_wsgi_app()
     port = env.get('PORT')
